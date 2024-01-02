4f07fe9aa7f84f031a5ccb19a5498f2f6958fe64
Robert Janetzko <robert_janetzko@web.de>
2024-01-02 11:32:50 +0100

    query for each

--- lib/qq.ml
+++ lib/qq.ml
@@ -1,29 +1,35 @@
 type 'a comp = (module Component.Sig with type t = 'a)
 
-type ('a, 'b) query =
-  | And : 'c comp * ('a, 'b) query -> ('c -> 'a, 'b) query
-  | Query : ('b, 'b) query
+type 'a query =
+  | And : 'c comp * 'a query -> ('c -> 'a) query
+  | Query : unit query
 
-let rec evaluate_query : type a b. int -> a -> (a, b) query -> b =
+let rec evaluate_query : type a. int -> a -> a query -> unit =
   fun id f args ->
   match args with
-  | And ((module M), r) -> evaluate_query id (f (M.get id)) r
   | Query -> f
+  | And ((module M), r) -> evaluate_query id (f (M.get id)) r
 ;;
+
+exception EmptyQuery
 
 let eval id q f = evaluate_query id f q
 let query m = And (m, Query)
 let query_with m q = And (m, q)
+let ( >& ) a b = query_with b a
 
-let () =
-  eval
-    123
-    (query (module Collision.Impact) |> query_with (module Default_components.Position))
-    (fun _p1 _p2 -> ())
+let for_each : type a. a query -> (int -> a) -> unit =
+  fun query f ->
+  let rec aux : type a. int -> a -> a query -> unit =
+    fun id f query ->
+    match query with
+    | Query -> f
+    | And ((module M), r) ->
+      (match M.get_opt id with
+       | None -> ()
+       | Some m -> aux id (f m) r)
+  in
+  match query with
+  | And ((module M), r) -> M.iter (fun id m -> aux id (f id m) r)
+  | _ -> raise EmptyQuery
 ;;
-
-let q1 = Query
-let q2 = And ((module Collision.Impact), q1)
-let q3 = And ((module Default_components.Position), q2)
-let q4 = And ((module Default_components.Velocity), q3)
-let () = eval 123 q3 (fun _p1 _p2 -> ())
